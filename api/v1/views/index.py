16bc38f0d219fe70e689d95bb012991023df41bf
Thomas Borde <thomas.borde94@hotmail.fr>
2023-04-12 01:11:08 -0700

    documented index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,11 @@
 
 #!/usr/bin/python3
-
+""" Index file"""
 from api.v1.views import app_views
 from flask import jsonify
 
 
 @app_views.route('/status')
+"""Testing of jsonify method"""
 def status():
     return (jsonify({'status':'OK'}))
