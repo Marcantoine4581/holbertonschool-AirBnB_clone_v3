37cdd8e25fcf4c036f6add893d53dd6a3b06ebdd
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-04-13 14:00:51 +0000

    import Review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -9,6 +9,7 @@ from models.city import City
 from models.state import State
 from models.place import Place
 from models.user import User
+from models.review import Review
 
 
 @app_views.route('/places/<place_id>/reviews', methods=['GET'],
