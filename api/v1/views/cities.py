f9d2a42b59a08802124fd62540496cc7f10a4820
Thomas Borde <thomas.borde94@hotmail.fr>
2023-04-13 03:57:40 -0700

    corrected a compilation error in cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-State file
+City file
 """
 from api.v1.views import app_views
 from flask import abort, jsonify, request, make_response
@@ -17,7 +17,7 @@ def get_cities(state_id):
     if not state:
         abort(404)
     for city in state.cities:
-        list_cities.append(city.to_dict())
+        cityList.append(city.to_dict())
     return jsonify(cityList)
 
 
