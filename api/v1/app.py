592a5cd2993ff6fb3da6d8ee59ce4eac87981e31
Thomas Borde <thomas.borde94@hotmail.fr>
2023-04-13 06:59:15 -0700

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,12 @@ from os import getenv
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+api_v1_cors_config = {
+    "origins": ["*"]
+    }
+CORS(app, resources={
+    r"/api/v1/*": api_v1_cors_config
+})
 """
 creates the flask application named app
 """
